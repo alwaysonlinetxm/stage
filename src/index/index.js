274a9f2b18490cce4551bc8742f7bc946b2431d8
taoxuming <taoxuming@hys-inc.cn>
2017-09-17 15:44:55 +0800

    update

--- src/index/index.js
+++ src/index/index.js
@@ -1,11 +1,12 @@
 import 'pixi.js';
 
+import Particle from './Particle';
 import dashboard from './dashboard';
 
 import '../libs/reset.scss';
 import './index.scss';
 
-import particleImg from './images/particle.png';
+
 
 // initial whole stage
 const canvas = document.querySelector('.canvas');
@@ -58,63 +59,22 @@ const RIGHT_WIDTH = CLIENT_WIDTH - LEFT_WIDTH;
 //
 // });
 
-const particles = new PIXI.Container();
-const particleTexture = PIXI.Texture.fromImage(particleImg);
-const particleList = [];
-const startX = 600;
-const endX = 300;
-const startY1 = 200;
-const endY1 = 350;
-const startY2 = 250;
-const endY2 = 600;
+const particle1 = new Particle({
+  startX: 600,
+  endX: 300,
+  startY1: 200,
+  endY1: 250,
+  startY2: 250,
+  endY2: 450,
+  sum: 200
+});
 
-const dir = startX > endX ? -1 : 1;
-const x1 = startX + (endX - startX) * 0.3;
-const x2 = startX + (endX - startX) * 0.6;
+window.p = particle1;
 
-for (let i = 0; i < 500; i++) {
-  setTimeout((function(i) {
-    return function() {
-      const particle = new PIXI.Sprite(particleTexture);
-      particle.anchor.set(0.5);
-      particle.x = startX;
-      particle.y = startY1 + (Math.random() * (startY2 - startY1));
-      particle.alpha = 0;
-      particle.scale.set(0.15);
-      // custom attr
-      particle.dis = dir * (0.5 + Math.random() * 0.5);
-
-      const endY = (particle.y - startY1) * (endY2 - startY2) / (endY1 - startY1) + startY2;
-      particle.k = (startX - endX) / (particle.y - endY);
-      particles.addChild(particle);
-      particleList.push(particle);
-    }
-  })(i), 50 * i);
-}
-
-app.stage.addChild(particles);
+app.stage.addChild(particle1.particles);
 
 app.ticker.add(function() {
-  particleList.forEach((node, i) => {
-    if (dir === 1 && node.x < endX || dir === -1 && node.x >= endX) {
-      const { dis, k } = node;
-
-      node.x += dis;
-      node.y += dis / k;
-      if (dir === 1 && node.x < x1 || dir === -1 && node.x >= x1) {
-        node.alpha = (node.x - startX) / (x1 - startX);
-      } else if (dir === 1 && node.x > x2 || dir === -1 && node.x <= x2) {
-        node.alpha = (endX - node.x ) / (endX - x2);
-      } else {
-        node.alpha = 1;
-      }
-    } else {
-      node.x = startX;
-      node.y = startY1 + (Math.random() * (startY2 - startY1));
-      node.alpha = 0;
-    }
-  });
-
+  particle1.animate()
 });
 
 const arrow = document.querySelector('.db-arrow');
---
+++ src/index/particle.js
@@ -0,0 +1,90 @@
+import 'pixi.js';
+
+import particleImg from './images/particle.png';
+
+export default class Particle {
+  constructor(opt) {
+    this.particles = new PIXI.Container();
+    this.particleTexture = PIXI.Texture.fromImage(particleImg);
+    this.particleList = [];
+    this.startX = opt.startX;
+    this.endX = opt.endX;
+    this.startY1 = opt.startY1;
+    this.endY1 = opt.endY1;
+    this.startY2 = opt.startY2;
+    this.endY2 = opt.endY2;
+    this.dir = this.startX > this.endX ? -1 : 1;
+    this.x1 = this.startX + (this.endX - this.startX) * 0.3;
+    this.x2 = this.startX + (this.endX - this.startX) * 0.6;
+
+    this.setSum(opt.sum);
+  }
+
+  setSum(n, interval) {
+    const { particles, particleList } = this;
+
+    if (particleList.length > 0 && n <= particleList.length) {
+      for (let i = n; i < particleList.length; i++) {
+        setTimeout((i => () => {
+          particles.removeChild(particleList[i]);
+        })(i), (interval || 50) * (i - n));
+      }
+    } else {
+      for (let i = particleList.length; i < n; i++) {
+        setTimeout((i => () => {
+          const particle = this._createParticle();
+          particles.addChild(particle);
+          particleList.push(particle);
+        })(i), (interval || 50) * (i - particleList.length));
+      }
+    }
+  }
+
+  setColor(color) {
+
+  }
+
+  animate() {
+    const { particles, particleList, startX, endX, startY1, startY2, endY1, endY2, x1, x2, dir } = this;
+
+    particleList.forEach((node, i) => {
+      if (dir === 1 && node.x < endX || dir === -1 && node.x >= endX) {
+        const { dis, k } = node;
+
+        node.x += dis;
+        node.y += dis / k;
+        if (dir === 1 && node.x < x1 || dir === -1 && node.x >= x1) {
+          node.alpha = (node.x - startX) / (x1 - startX);
+        } else if (dir === 1 && node.x > x2 || dir === -1 && node.x <= x2) {
+          node.alpha = (endX - node.x ) / (endX - x2);
+        } else {
+          node.alpha = 1;
+        }
+      } else {
+        node.x = startX;
+        node.y = startY1 + (Math.random() * (startY2 - startY1));
+        node.alpha = 0;
+      }
+    });
+  }
+
+  _createParticle() {
+    const { startX, endX, startY1, startY2, endY1, endY2, dir } = this;
+    const particle = new PIXI.Graphics();
+
+    particle.lineStyle(0);
+    particle.beginFill(0x709bf0, 0.5);
+    particle.drawCircle(0, 0, 1);
+    particle.endFill();
+    particle.x = startX;
+    particle.y = startY1 + (Math.random() * (startY2 - startY1));
+    particle.alpha = 0;
+    // custom attr
+    particle.dis = dir * (0.5 + Math.random() * 0.5);
+
+    const endY = (particle.y - startY1) * (endY2 - startY2) / (endY1 - startY1) + startY2;
+    particle.k = (startX - endX) / (particle.y - endY);
+
+    return particle;
+  }
+}
