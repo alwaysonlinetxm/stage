2c13ec5e5e1edfa0fe2ab98bf9abd5309be3c1b6
taoxuming <taoxuming@hys-inc.cn>
2017-09-20 21:22:38 +0800

    update

--- src/index/index.js
+++ src/index/index.js
@@ -123,7 +123,7 @@ function updateState(temp, sum, dir) {
 
 let temp = 16 + Math.ceil(16 * Math.random());
 let sum =  Math.ceil(100 * Math.random());
-let dir = '内循环';
+let dir = '外循环';
 
 updateState(temp, sum, dir);
 
@@ -133,11 +133,11 @@ setInterval(() => {
   // });
   temp = 16 + Math.ceil(16 * Math.random());
   sum =  Math.ceil(100 * Math.random());
-  // if (dir === '内循环') {
-  //   dir = '外循环';
-  // } else {
-  //   dir = '内循环';
-  // }
+  if (dir === '内循环') {
+    dir = '外循环';
+  } else {
+    dir = '内循环';
+  }
 
   updateState(temp, sum, dir);
 }, 10000);
