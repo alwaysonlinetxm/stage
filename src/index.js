09c09c2073f93b7732617ce11bc2546138fb748f
taoxuming <taoxuming@hys-inc.cn>
2017-09-26 16:27:46 +0800

    add blue line

--- src/index.js
+++ src/index.js
@@ -25,26 +25,34 @@ forward.addEventListener('click', () => {
   }
 }, false);
 
-
+let mode = Math.ceil(Math.random() * 7);
 let temp = 16 + Math.ceil(16 * Math.random());
 let sum =  Math.ceil(100 * Math.random());
 let dir = '外循环';
+let blue = -4;
+let red = 85;
 
 updatePage1(temp, sum, dir);
-
+updatePage2(mode, temp, sum, blue, red);
 
 
 setInterval(() => {
-  // Util.request('S2=&S3=&S4=').then(res => {
+  // Util.request('S1=&S2=&S3=&S4=&S5=&S6=').then(res => {
   //
   // });
+  mode = Math.ceil(Math.random() * 7);
   temp = 16 + Math.ceil(16 * Math.random());
   sum =  Math.ceil(100 * Math.random());
+
   if (dir === '内循环') {
     dir = '外循环';
   } else {
     dir = '内循环';
   }
 
+  blue = Math.ceil(Math.random() * 125) - 40;
+  red = Math.ceil(Math.random() * 125) - 40;
+
   updatePage1(temp, sum, dir);
+  updatePage2(mode, temp, sum, blue, red);
 }, 10000);
--- src/page2/index.js
+++ src/page2/index.js
@@ -1,5 +1,9 @@
 import './index.scss';
 
-export function updatePage2() {
+const blueNum = document.querySelector('.page2 .blue .num');
+const redNum = document.querySelector('.page2 .red .num');
 
+export function updatePage2(mode, temp, sum, blue, red) {
+  blueNum.innerHTML = `${blue}℃`;
+  redNum.innerHTML = `${red}℃`;
 }
