d841169a42b4e2746823487784e299547b607d2a
taoxuming <taoxuming@hys-inc.cn>
2017-09-29 19:23:27 +0800

    update

--- src/page2/index.js
+++ src/page2/index.js
@@ -13,6 +13,7 @@ const arrows3 = document.querySelector('.page2 .arrows3');
 const arrows4 = document.querySelector('.page2 .arrows4');
 const arrows5 = document.querySelector('.page2 .arrows5');
 const arrows6 = document.querySelector('.page2 .arrows6');
+const arrows7 = document.querySelector('.page2 .arrows7');
 
 function updateArrows1(sum) {
   const levels = Math.ceil(sum / 10);
@@ -170,6 +171,33 @@ function updateArrows6(temp, sum) {
   }
 }
 
+function updateArrows7(temp, sum) {
+  const levels = Math.ceil(sum / 20);
+  const polygons = arrows7.querySelectorAll('svg');
+
+  polygons.forEach(node => arrows7.removeChild(node));
+
+  for (let i = 0; i < levels; i++) {
+    const polygon = document.createElementNS('http://www.w3.org/2000/svg','svg');
+    polygon.setAttribute('width', CLIENT_WIDTH * 0.093);
+    polygon.setAttribute('height', CLIENT_WIDTH * 0.195);
+    polygon.style.top = `-${i * 2}%`;
+    polygon.style.left = `-${i * 3}%`;
+
+    polygon.innerHTML = `
+      <polygon points="20,5 14,0 14,2 0,2 0,7 14,7 14,9" style="fill:${COLORS[temp]}">
+        <animateMotion path="M110,50 q-40,100 -50,200" begin="0s" dur="2s" rotate="auto" repeatCount="indefinite"/>
+        <animate attributeName="opacity" begin="0s" dur="2s" values="0;1;1;0" repeatCount="indefinite" />
+      </polygon>
+    `;
+    if (i % 2 === 1) {
+      setTimeout(() => arrows7.appendChild(polygon), 1000);
+    } else {
+      arrows7.appendChild(polygon);
+    }
+  }
+}
+
 export function updatePage2(mode, temp, sum, blue, red) {
   blueNum.innerHTML = `${blue}℃`;
   redNum.innerHTML = `${red}℃`;
@@ -203,4 +231,5 @@ export function updatePage2(mode, temp, sum, blue, red) {
   updateArrows4(sum);
   updateArrows5(intTemp, sum);
   updateArrows6(intTemp, sum);
+  updateArrows7(intTemp, sum);
 }
