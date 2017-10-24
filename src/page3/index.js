cf54fffbe0a453b3d30979ed1880d9725c161d89
alwaysonlinetxm <alwaysonlinetxm@163.com>
2017-10-24 10:29:00 +0800

    update

--- src/page3/index.js
+++ src/page3/index.js
@@ -1,6 +1,25 @@
 import './index.scss';
 
-const CLIENT_WIDTH = window.innerWidth;
+const topList = document.querySelectorAll('.page3 .top-item');
+const snow = topList[0];
+const circle = topList[1];
+const auto = topList[2];
+const ion = topList[3];
+const leftUp = document.querySelector('.page3 .left-temp .up-arrow');
+const leftDown = document.querySelector('.page3 .left-temp .down-arrow');
+const leftTemp = document.querySelector('.page3 .left-temp .temp');
+const rightUp = document.querySelector('.page3 .right-temp .up-arrow');
+const rightDown = document.querySelector('.page3 .right-temp .down-arrow');
+const rightTemp = document.querySelector('.page3 .right-temp .temp');
+const minus = document.querySelector('.page3 .minus');
+const plus = document.querySelector('.page3 .plus');
+const sum = document.querySelector('.page3 .sum');
+const downList = document.querySelectorAll('.page3 .down-item');
+const vent = downList[0];
+const vf = downList[1];
+const floor = downList[2];
+const shield = downList[3];
+const sync = document.querySelector('.page3 .sync');
 
 export function updatePage3(mode, temp, sum, dir) {
 
