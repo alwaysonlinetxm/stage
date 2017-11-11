06fe9f35920352b19496f719de6a156b6ccad510
alwaysonlinetxm <alwaysonlinetxm@163.com>
2017-11-11 18:14:49 +0800

    update

--- src/page2/index.js
+++ src/page2/index.js
@@ -16,7 +16,7 @@ const arrows6 = document.querySelector('.page2 .arrows6');
 const arrows7 = document.querySelector('.page2 .arrows7');
 
 function updateArrows1(sum) {
-  const levels = Math.ceil(sum / 10);
+  const levels = Math.ceil(sum / 5);
   const polygons = arrows1.querySelectorAll('svg');
 
   polygons.forEach(node => arrows1.removeChild(node));
@@ -37,164 +37,146 @@ function updateArrows1(sum) {
 }
 
 function updateArrows2(sum) {
-  const levels = Math.ceil(sum / 10);
+  const levels = Math.ceil(sum / 5);
   const polygons = arrows2.querySelectorAll('svg');
 
   polygons.forEach(node => arrows2.removeChild(node));
 
-  for (let i = 0; i < levels; i++) {
+  for (let i = 1; i <= levels; i++) {
     const polygon = document.createElementNS('http://www.w3.org/2000/svg','svg');
     polygon.setAttribute('width', CLIENT_WIDTH * 0.125);
     polygon.setAttribute('height', CLIENT_WIDTH * 0.234);
-    polygon.style.top = `-${i / 2 * 4}%`;
-    polygon.style.left = `${i / 2 * 3}%`;
-
     polygon.innerHTML = `
       <polygon points="20,5 14,0 14,2 0,2 0,7 14,7 14,9" style="fill:#8E1925">
         <animateMotion path="M140,280 q-190,-80 -110,-240" begin="0s" dur="3s" rotate="auto" repeatCount="indefinite"/>
         <animate attributeName="opacity" begin="0s" dur="3s" values="0;1;1;1;0" repeatCount="indefinite" />
       </polygon>
     `;
-    if (i % 2 === 1) {
-      setTimeout(() => arrows2.appendChild(polygon), 1500);
-    } else {
-      arrows2.appendChild(polygon);
-    }
+    const layerNum = Math.ceil(levels / 4);
+    const layer = i % layerNum;
+    polygon.style.top = `-${layer * 4}%`;
+    polygon.style.left = `${layer * 2}%`;
+    setTimeout(() => arrows2.appendChild(polygon), Math.floor(i / layerNum) * 750 );
   }
 }
 
 function updateArrows3(sum) {
-  const levels = Math.ceil(sum / 20);
+  const levels = Math.ceil(sum / 10);
   const polygons = arrows3.querySelectorAll('svg');
 
   polygons.forEach(node => arrows3.removeChild(node));
 
-  for (let i = 0; i < levels; i++) {
+  for (let i = 1; i <= levels; i++) {
     const polygon = document.createElementNS('http://www.w3.org/2000/svg','svg');
     polygon.setAttribute('width', CLIENT_WIDTH * 0.094);
     polygon.setAttribute('height', CLIENT_WIDTH * 0.117);
-    polygon.style.top = `-${i * 2}%`;
-    polygon.style.left = `${i * 1}%`;
-
     polygon.innerHTML = `
       <polygon points="20,5 14,0 14,2 0,2 0,7 14,7 14,9" style="fill:#061e81">
         <animateMotion path="M100,90 q-100,-40 -90,-80" begin="0s" dur="1.5s" rotate="auto" repeatCount="indefinite"/>
         <animate attributeName="opacity" begin="0s" dur="1.5s" values="0;1;1;0" repeatCount="indefinite" />
       </polygon>
     `;
-    if (i % 2 === 1) {
-      setTimeout(() => arrows3.appendChild(polygon), 750);
-    } else {
-      arrows3.appendChild(polygon);
-    }
+    const layerNum = Math.ceil(levels / 3);
+    const layer = i % layerNum;
+    polygon.style.top = `-${layer * 4}%`;
+    polygon.style.left = `${layer * 3}%`;
+    setTimeout(() => arrows3.appendChild(polygon), Math.floor(i / layerNum) * 500 );
   }
 }
 
 function updateArrows4(sum) {
-  const levels = Math.ceil(sum / 20);
+  const levels = Math.ceil(sum / 10);
   const polygons = arrows4.querySelectorAll('svg');
 
   polygons.forEach(node => arrows4.removeChild(node));
 
-  for (let i = 0; i < levels; i++) {
+  for (let i = 1; i <= levels; i++) {
     const polygon = document.createElementNS('http://www.w3.org/2000/svg','svg');
-    polygon.setAttribute('width', CLIENT_WIDTH * 0.094);
+    polygon.setAttribute('width', CLIENT_WIDTH * 0.1);
     polygon.setAttribute('height', CLIENT_WIDTH * 0.117);
-    polygon.style.top = `${i * 2}%`;
-    polygon.style.left = `${i * 1}%`;
-
     polygon.innerHTML = `
       <polygon points="20,5 14,0 14,2 0,2 0,7 14,7 14,9" style="fill:#061e81">
-        <animateMotion path="M100,20 q-105,20 -90,80" begin="0s" dur="1.5s" rotate="auto" repeatCount="indefinite"/>
+        <animateMotion path="M120,20 q-115,80 -90,80" begin="0s" dur="1.5s" rotate="auto" repeatCount="indefinite"/>
         <animate attributeName="opacity" begin="0s" dur="1.5s" values="0;1;1;0" repeatCount="indefinite" />
       </polygon>
     `;
-    if (i % 2 === 1) {
-      setTimeout(() => arrows4.appendChild(polygon), 750);
-    } else {
-      arrows4.appendChild(polygon);
-    }
+    const layerNum = Math.ceil(levels / 3);
+    const layer = i % layerNum;
+    polygon.style.top = `-${layer * 8}%`;
+    polygon.style.left = `${layer * 2}%`;
+    setTimeout(() => arrows4.appendChild(polygon), Math.floor(i / layerNum) * 500 );
   }
 }
 
 function updateArrows5(temp, sum) {
-  const levels = Math.ceil(sum / 20);
+  const levels = Math.ceil(sum / 10);
   const polygons = arrows5.querySelectorAll('svg');
 
   polygons.forEach(node => arrows5.removeChild(node));
 
-  for (let i = 0; i < levels; i++) {
+  for (let i = 1; i <= levels; i++) {
     const polygon = document.createElementNS('http://www.w3.org/2000/svg','svg');
     polygon.setAttribute('width', CLIENT_WIDTH * 0.062);
     polygon.setAttribute('height', CLIENT_WIDTH * 0.093);
-    polygon.style.top = `-${i * 2}%`;
-    polygon.style.left = `${i * 2}%`;
-
     polygon.innerHTML = `
       <polygon points="20,5 14,0 14,2 0,2 0,7 14,7 14,9" style="fill:${COLORS[temp]}">
         <animateMotion path="M0,110 q0,-50 20,-100 " begin="0s" dur="1s" rotate="auto" repeatCount="indefinite"/>
         <animate attributeName="opacity" begin="0s" dur="1s" values="0;1;1;0" repeatCount="indefinite" />
       </polygon>
     `;
-    if (i % 2 === 1) {
-      setTimeout(() => arrows5.appendChild(polygon), 500);
-    } else {
-      arrows5.appendChild(polygon);
-    }
+    const layerNum = Math.ceil(levels / 2);
+    const layer = i % layerNum;
+    polygon.style.top = `-${layer * 2}%`;
+    polygon.style.left = `${layer * 3}%`;
+    setTimeout(() => arrows5.appendChild(polygon), Math.floor(i / layerNum) * 500 );
   }
 }
 
 function updateArrows6(temp, sum) {
-  const levels = Math.ceil(sum / 20);
+  const levels = Math.ceil(sum / 10);
   const polygons = arrows6.querySelectorAll('svg');
 
   polygons.forEach(node => arrows6.removeChild(node));
 
-  for (let i = 0; i < levels; i++) {
+  for (let i = 1; i <= levels; i++) {
     const polygon = document.createElementNS('http://www.w3.org/2000/svg','svg');
     polygon.setAttribute('width', CLIENT_WIDTH * 0.062);
     polygon.setAttribute('height', CLIENT_WIDTH * 0.101);
-    polygon.style.top = `${i * 1}%`;
-    polygon.style.left = `-${i * 3}%`;
-
     polygon.innerHTML = `
       <polygon points="20,5 14,0 14,2 0,2 0,7 14,7 14,9" style="fill:${COLORS[temp]}">
         <animateMotion path="M70,120 q-20,-50 -20,-100 " begin="0s" dur="1s" rotate="auto" repeatCount="indefinite"/>
         <animate attributeName="opacity" begin="0s" dur="1s" values="0;1;1;0" repeatCount="indefinite" />
       </polygon>
     `;
-    if (i % 2 === 1) {
-      setTimeout(() => arrows6.appendChild(polygon), 500);
-    } else {
-      arrows6.appendChild(polygon);
-    }
+    const layerNum = Math.ceil(levels / 2);
+    const layer = i % layerNum;
+    polygon.style.top = `-${layer * 2}%`;
+    polygon.style.left = `${layer * 3}%`;
+    setTimeout(() => arrows6.appendChild(polygon), Math.floor(i / layerNum) * 500 );
   }
 }
 
 function updateArrows7(temp, sum) {
-  const levels = Math.ceil(sum / 20);
+  const levels = Math.ceil(sum / 5);
   const polygons = arrows7.querySelectorAll('svg');
 
   polygons.forEach(node => arrows7.removeChild(node));
 
-  for (let i = 0; i < levels; i++) {
+  for (let i = 1; i <= levels; i++) {
     const polygon = document.createElementNS('http://www.w3.org/2000/svg','svg');
     polygon.setAttribute('width', CLIENT_WIDTH * 0.093);
-    polygon.setAttribute('height', CLIENT_WIDTH * 0.195);
-    polygon.style.top = `-${i * 2}%`;
-    polygon.style.left = `-${i * 3}%`;
-
+    polygon.setAttribute('height', CLIENT_WIDTH * 0.21);
     polygon.innerHTML = `
       <polygon points="20,5 14,0 14,2 0,2 0,7 14,7 14,9" style="fill:${COLORS[temp]}">
-        <animateMotion path="M110,50 q-40,100 -50,200" begin="0s" dur="2s" rotate="auto" repeatCount="indefinite"/>
+        <animateMotion path="M110,50 q-80,100 -70,200" begin="0s" dur="2s" rotate="auto" repeatCount="indefinite"/>
         <animate attributeName="opacity" begin="0s" dur="2s" values="0;1;1;0" repeatCount="indefinite" />
       </polygon>
     `;
-    if (i % 2 === 1) {
-      setTimeout(() => arrows7.appendChild(polygon), 1000);
-    } else {
-      arrows7.appendChild(polygon);
-    }
+    const layerNum = Math.ceil(levels / 4);
+    const layer = i % layerNum;
+    polygon.style.top = `-${layer * 2}%`;
+    polygon.style.left = `${layer * 3}%`;
+    setTimeout(() => arrows7.appendChild(polygon), Math.floor(i / layerNum) * 500 );
   }
 }
 
